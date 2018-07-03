7fa764c7dbda07331c3f7e0da4d2439127ca31f1
Antoine LEFEBVRE <alefebvr@e3r12p22.42.fr>
2018-07-03 05:07:49 +0200

    adds

--- back/public_profile.js
+++ back/public_profile.js
@@ -1,15 +1,15 @@
-function	checklike(user_id, like_id, callback)
+function	checklike(user_id, his_id, callback)
 {
 	var a = 0
 	var b = 0
-	 con.query('SELECT * FROM likes WHERE user_id = ? AND like_id = ?', [user_id, like_id], function (err, rows) { if (err) throw err 
+	 con.query('SELECT * FROM likes WHERE user_id = ? AND his_id = ?', [user_id, his_id], function (err, rows) { if (err) throw err 
 	 	if (rows.length == 1)
 	 		a = 1;
 	 	
-	 	con.query('SELECT * FROM likes WHERE user_id = ? AND like_id = ?', [like_id, user_id], function (err, rows) { if (err) throw err 
+	 	con.query('SELECT * FROM likes WHERE user_id = ? AND his_id = ?', [his_id, user_id], function (err, rows) { if (err) throw err 
 	 	if (rows.length == 1)
 	 		b = 1;
-	 	if (user_id == like_id)
+	 	if (user_id == his_id)
 	 		return callback(-1);
 	 	else if (a == 0 && b == 0)
 	 		return callback(0);
@@ -22,7 +22,64 @@ function	checklike(user_id, like_id, callback)
 	 }) })
 }
 
+function	check(table, user_id, his_id, callback)
+{
+	con.query('SELECT * FROM ' + table + ' WHERE user_id = ? AND his_id = ?', [user_id, his_id], function (err, rows) { if (err) throw err 
+		if (rows.length == 0)
+			return callback(0);
+		else
+			return callback(1);
+	})
+}
+function	insertinto(table)
+{
+	con.query('INSERT INTO ' + table + ' (user_id, his_id) VALUES (?,?) ', [req.session.profile.id, req.params.id], function (err) { if (err) throw err })
+}
+function	deletefrom(table)
+{
+	con.query('DELETE FROM ' + table + ' WHERE user_id = ? AND his_id = ?', [req.session.profile.id, req.params.id], function (err) { if (err) throw err })
+}
+
+if (req.body.like)
+{
+	checklike(req.session.profile.id, req.params.id, function(like){
+		if (like == -1 || like == 1 || like == 3)
+			return ;
+		else
+			insertinto('likes')
+	})
+}
+if (req.body.dislike)
+{
+	checklike(req.session.profile.id, req.params.id, function(like){
+		if (like == 0 || like == 2)
+			return ;
+		else
+			deletefrom('likes')
+	})
+}
+if (req.body.block)
+{
+	check('block', req.session.profile.id, req.params.id, function(block){
+		if (block == 0)
+			insertinto('block')
+		else
+			deletefrom('block')
+	})
+}
+if (req.body.report)
+{
+	check('report', req.session.profile.id, req.params.id, function(report){
+		if (report == 0)
+			insertinto('report')
+		else
+			deletefrom('report')
+	})
+}
+
 con.query('SELECT * FROM users WHERE id = ?', [req.params.id], function (err, result) { if (err) throw err
 con.query('SELECT * FROM tags WHERE user_id = ?', [req.params.id], function (err, resultag) { if (err) throw err 
-checklike(req.session.profile.id, result[0].id, function(like) {
-res.render('public_profile.ejs', {req: req, css: css, like: like, profile: result[0], tag: resultag }) }) }) })
+checklike(req.session.profile.id, req.params.id, function(like) {
+check('block', req.session.profile.id, req.params.id, function(block){
+check('report', req.session.profile.id, req.params.id, function(report){
+res.render('public_profile.ejs', {req: req, css: css, like: like, block: block, report: report, profile: result[0], tag: resultag }) }) }) }) }) })
--- server.js
+++ server.js
@@ -54,19 +54,31 @@ con.connect(function(err) { if (err) throw err
         img3 VARCHAR(255) DEFAULT 'empty.png', \
         img4 VARCHAR(255) DEFAULT 'empty.png', \
         img5 VARCHAR(255) DEFAULT 'empty.png')`
-    con.query(users, function (err, res) { if (err) throw err })
+    con.query(users, function (err) { if (err) throw err })
 
     var tags = `CREATE TABLE IF NOT EXISTS tags ( \
         id INT AUTO_INCREMENT PRIMARY KEY, \
         user_id INT, \
         tag VARCHAR(255))`
-    con.query(tags, function (err, res) { if (err) throw err }) 
+    con.query(tags, function (err) { if (err) throw err }) 
     
     var likes = `CREATE TABLE IF NOT EXISTS likes (\
         id INT AUTO_INCREMENT PRIMARY KEY, \
         user_id INT NOT NULL, \ 
-        like_id INT NOT NULL)`
-    con.query(likes, function (err, res) { if (err) throw err }) 
+        his_id INT NOT NULL)`
+    con.query(likes, function (err) { if (err) throw err }) 
+    
+    var block = `CREATE TABLE IF NOT EXISTS block (\
+        id INT AUTO_INCREMENT PRIMARY KEY, \
+        user_id INT NOT NULL, \ 
+        his_id INT NOT NULL)`
+    con.query(block, function (err) { if (err) throw err }) 
+    
+    var report = `CREATE TABLE IF NOT EXISTS report (\
+        id INT AUTO_INCREMENT PRIMARY KEY, \
+        user_id INT NOT NULL, \ 
+        his_id INT NOT NULL)`
+    con.query(report, function (err) { if (err) throw err }) 
 })
 
 server.use(express.static(__dirname + '/img'))
@@ -110,7 +122,7 @@ server.get('/', function(req,res){
         res.render('profile.ejs', {req: req, css: css, error: 'none', profile: req.session.profile})
 })
 .get('/public_profile', function(req,res){
-  res.render('public_profile.ejs', {req: req, like: -1, css: css, profile: req.session.profile, tag: req.session.profile.tag})
+  res.render('public_profile.ejs', {req: req, like: -1, block: 0, report: 0, css: css, profile: req.session.profile, tag: req.session.profile.tag})
 })
 .post('/peers', function(req, res) {
     eval(fs.readFileSync(__dirname + "/back/peers.js")+'')
